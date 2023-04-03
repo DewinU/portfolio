e372cdd1d80e17f017e063d42b338ade28711d99
DewinU <umana.dewin@gmail.com>
2023-04-03 13:46:23 -0600

    Linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={inter.className}>
-      <AboutMe/>
+      <AboutMe />
       <Skills />
       <Portofilo />
     </main>
--- src/components/sections/aboutMe.tsx
+++ src/components/sections/aboutMe.tsx
@@ -1,16 +1,15 @@
-export default function AboutMe(){
-    const Subtitle: string = "I'm a FullStack Developer with a passion for building dynamic and engaging web applications and API's with the latest technologies."
-    return (
-        <section>
-        <div className='flex flex-col items-center p-10 text-center'>
-            <h1 className='p-5 text-4xl font-extrabold'>About Me</h1>
-            <p className='text-2xl'>
-            {Subtitle}
-            </p>
-        </div>
-        <div className='flex flex-col items-center text-center '>
-            <p className='text-2xl'>🚧 Under Construction 🚧</p>
-        </div>
-        </section>
-    )
+export default function AboutMe() {
+  const Subtitle: string =
+    "I'm a FullStack Developer with a passion for building dynamic and engaging web applications and API's with the latest technologies."
+  return (
+    <section>
+      <div className='flex flex-col items-center p-10 text-center'>
+        <h1 className='p-5 text-4xl font-extrabold'>About Me</h1>
+        <p className='text-2xl'>{Subtitle}</p>
+      </div>
+      <div className='flex flex-col items-center text-center '>
+        <p className='text-2xl'>🚧 Under Construction 🚧</p>
+      </div>
+    </section>
+  )
 }
