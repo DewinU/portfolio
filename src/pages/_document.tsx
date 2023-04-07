fb608f02ab8fd621ab15731a1fbfcd0722dab34d
DewinU <umana.dewin@gmail.com>
2023-04-07 15:52:03 -0600

    Tag manager deleted

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,23 +15,23 @@ export default function Document() {
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
         </Script>
-        <Script id='google-tag-manager' strategy='worker'>
+        {/* <Script id='google-tag-manager' strategy='worker'>
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-59LQQWQ');`}
-        </Script>
+        </Script> */}
       </Head>
       <body>
-        <noscript>
+        {/* <noscript>
           <iframe
             src='https://www.googletagmanager.com/ns.html?id=GTM-59LQQWQ'
             height='0'
             width='0'
             className='invisible hidden'
           />
-        </noscript>
+        </noscript> */}
         <Main />
         <NextScript />
       </body>
