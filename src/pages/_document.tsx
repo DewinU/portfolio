49594584792ec4205932ce90372baac7d9ddb2c7
DewinU <umana.dewin@gmail.com>
2023-04-07 22:08:25 -0600

    Hotjar deleted

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,19 +6,8 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        {/* <Script
-          id='tag-manager'
-          strategy='worker'
-          async
-          src='https://www.googletagmanager.com/gtag/js?id=G-YE9GWTLB88'/>
-        <Script
-          id='google-analytics'
-          strategy='worker'>{`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-YE9GWTLB88');`}</Script> */}
-        <Script id='hotjar' strategy='worker'>
+        {/* Hotjar Code */}
+        {/* <Script id='hotjar' strategy='worker'>
           {`(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:3272577,hjsv:6};
@@ -27,14 +16,14 @@ export default function Document() {
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-        </Script>
+        </Script> */}
       </Head>
       <body>
-      <noscript>
+        <noscript>
           <iframe
             src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
+            height='0'
+            width='0'
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
