a6c1446268ae4d72212cab1ac943920e4944a46c
DewinU <umana.dewin@gmail.com>
2023-04-07 21:58:53 -0600

    added tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,36 @@
 import '@/styles/globals.css'
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import Layout from './layout'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { GTM_ID, pageview } from '../lib/gtm'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    router.events.on('routeChangeComplete', pageview)
+    return () => {
+      router.events.off('routeChangeComplete', pageview)
+    }
+  }, [router.events])
   return (
     <>
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        id='gtag-base'
+        strategy='worker'
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
+import { GTM_ID } from '../lib/gtm'
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <Script
+        {/* <Script
           id='tag-manager'
           strategy='worker'
           async
-          src='https://www.googletagmanager.com/gtag/js?id=G-YE9GWTLB88'></Script>
+          src='https://www.googletagmanager.com/gtag/js?id=G-YE9GWTLB88'/>
         <Script
           id='google-analytics'
           strategy='worker'>{`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
       
-        gtag('config', 'G-YE9GWTLB88');`}</Script>
+        gtag('config', 'G-YE9GWTLB88');`}</Script> */}
         <Script id='hotjar' strategy='worker'>
           {`(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
@@ -27,23 +28,16 @@ export default function Document() {
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
         </Script>
-        {/* <Script id='google-tag-manager' strategy='worker'>
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-59LQQWQ');`}
-        </Script> */}
       </Head>
       <body>
-        {/* <noscript>
+      <noscript>
           <iframe
-            src='https://www.googletagmanager.com/ns.html?id=GTM-59LQQWQ'
-            height='0'
-            width='0'
-            className='invisible hidden'
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
           />
-        </noscript> */}
+        </noscript>
         <Main />
         <NextScript />
       </body>
